996dba8de6704e01340a480f6b563ffbdb22d370
Viítor Hugo Tirado Peñaranda <victor.tirado@fundacion-jala.org>
2021-04-19 23:43:33 -0400

    clases del ToDo fueron creados

---
+++ index.js
@@ -0,0 +1,101 @@
+window.onload = ()=>{
+    listado = new ListadoTareas();
+}
+
+
+
+
+class Tarea{
+
+    
+    constructor(nombre, descripcion, prioridad, responsables, etiquetas, proyectos, fVencimiento ){
+        this.id = this.generarId();
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.prioridad = prioridad;
+        this.estado = 'Pendiente';
+        this.responsables = responsables;
+        this.etiquetas = etiquetas;
+        this,proyectos = proyectos;
+        this.fCreacion = new Date();
+        this.fInicio = null;
+        this.fFinalizacion = null;
+        this.fVencimiento = fVencimiento;
+    }
+    
+    
+        generarId(){
+            return Math.floor(Math.random() * 989684964); 
+        }
+    
+    
+    setNombre(nombre){
+        this.nombre = nombre;
+    }
+
+    getNombre(){
+        return this.nombre;
+    }
+
+    setEstado(estado){
+        this.estado = estado;
+    }
+  
+}
+
+class ListadoTareas{
+    constructor(){
+        this.lista = [];
+    }
+
+    mostrarListado(){
+        return this.lista;
+    }
+
+    mostrarTarea(id){
+        let indice = this.lista.findIndex( (elemento) => elemento.id === id );
+        if(indice === -1){
+            return [];
+        }
+        else{
+            return this.lista[indice];
+        }
+
+    }
+
+    agregarTarea(tarea){
+        this.lista.push(tarea);
+    }
+
+    eliminarTarea(id){
+        let indice = this.lista.findIndex( (elemento) => elemento.id === id );
+        if(indice === -1){
+            return false;
+        }
+        else{
+            this.lista.splice(indice, 1);
+            return true;
+        }
+    }
+
+    actualizarTarea(id, tarea){
+        let indice = this.lista.findIndex( (elemento) => elemento.id === id );
+        if(indice === -1){
+            return false;
+        }
+        else{
+            this.lista.splice(indice, 1, tarea);
+            return true;
+        }
+    }
+
+    guardar(){
+        localStorage.setItem('datos', JSON.stringify(this.lista))
+    }
+
+    recuperar(){
+        this.lista = JSON.parse(localStorage.getItem('datos'))
+    }
+}
+
+
